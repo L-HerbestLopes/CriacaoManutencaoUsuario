b8259353dc3cf17cc4bbc82d096f5678aca17ce0
YuriSousaAlmeida <yurisousaalmeida2004@gmail.com>
2025-12-08 17:01:33 -0300

    Conexão banco de dados SQLite
    Modificação do pom.xml para configuração do maven para receber as configurações necessárias para conectar ao banco de dados SQLite. Pacote próprio do banco de dados criado e a implementação da classe DatabaseConnection.

--- src/main/java/com/pss/criacaomanutencaousuarios/Principal.java
+++ src/main/java/com/pss/criacaomanutencaousuarios/Principal.java
@@ -1,10 +1,15 @@
 package com.pss.criacaomanutencaousuarios;
 
+import com.pss.criacaomanutencaousuarios.database.DatabaseConnection;
 import com.pss.criacaomanutencaousuarios.model.Usuario;
 import com.pss.criacaomanutencaousuarios.model.UsuarioRepository;
 import com.pss.criacaomanutencaousuarios.presenter.FormularioUsuarioPresenter;
 import com.pss.criacaomanutencaousuarios.presenter.SistemaPresenter;
 import java.util.ArrayList;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 /**
  * @author André Tavares Louzada, Lucas Herbest Lopes e Yuri Sousa Almeida
@@ -12,6 +17,8 @@ import java.util.ArrayList;
 public class Principal {
 
     public static void main(String[] args) {
+        listTables(DatabaseConnection.connect());
+        
         SistemaPresenter sistema = SistemaPresenter.getInstancia();
         
         ArrayList<Usuario> usuarios = new ArrayList<>();
@@ -21,4 +28,34 @@ public class Principal {
         FormularioUsuarioPresenter formulario = new FormularioUsuarioPresenter("Editar usuário");
         sistema.abrirJanela(formulario);
     }
+    
+	    
+    public static void listTables(Connection conn) {
+        // Consulta o sqlite_master para encontrar todas as tabelas
+        String sql = "SELECT name FROM sqlite_master WHERE type='table' AND name NOT LIKE 'sqlite_%'";
+
+        try (Statement stmt = conn.createStatement();
+             ResultSet rs = stmt.executeQuery(sql)) {
+
+            System.out.println("\n--- TABELAS EXISTENTES NO BANCO ---");
+            boolean found = false;
+            
+            while (rs.next()) {
+                String tableName = rs.getString("name");
+                System.out.println("Tabela: " + tableName);
+                found = true;
+            }
+            
+            if (!found) {
+                 System.out.println("Nenhuma tabela de usuário encontrada.");
+            }
+
+        } catch (SQLException e) {
+            System.err.println("Erro ao listar as tabelas: " + e.getMessage());
+        }
+    }
+
 }
+
+
+
